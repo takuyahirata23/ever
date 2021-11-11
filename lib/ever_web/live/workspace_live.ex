cba030e5133fcce38924a2997ede49c78929202e
Takuya Hirata <takuyahirata4@gmail.com>
2021-11-11 08:24:54 -0500

    create task button

--- lib/ever_web/live/workspace_live.ex
+++ lib/ever_web/live/workspace_live.ex
@@ -6,7 +6,7 @@ defmodule EverWeb.WorkspaceLive do
   def mount(%{"workspace_id" => id}, _session, socket) do
     workspace = Workspaces.read_workspace(id)
 
-    {:ok, assign(socket, workspace: workspace)}
+    {:ok, assign(socket, workspace: workspace, show_modal: false)}
   end
 
   def render(assigns) do
@@ -15,7 +15,37 @@ defmodule EverWeb.WorkspaceLive do
     <h1 class="font-bold text-3xl">
     <%= @workspace.name %>
     </h1>
+    <div>
+    <button 
+    phx-click="toggle-modal" 
+    class="p-2 bg-blue-500 rounded-md text-white text-sm"
+    >
+    Create Task
+    </button>
+    </div>
+        <%= if @show_modal do %>
+          <div class="phx-modal" 
+            phx-window-keydown="toggle-modal"
+            phx-key="escape"
+            phx-capture-click="toggle-modal"
+          >
+            <div class="phx-modal-content w-1/2">
+              <div class="phx-modal-close">
+                <a href="#" phx-click="toggle-modal">
+                  &times;
+                </a>
+              </div>
+              <div>
+                 hiii
+              </div>
+            </div>
+          </div>
+        <% end %>
     </section>
     """
+  end
+
+  def handle_event("toggle-modal", _, socket) do
+    {:noreply, update(socket, :show_modal, &(!&1))}
   end
 end
